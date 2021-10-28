e8e4c70b2684421cb9a2277b8b1b787795c6a9eb
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-10-28 09:11:21 +0900

    서버와 통신할 준비

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NameEditorComponent } from './name-editor/name-editor.component';
@@ -9,7 +9,12 @@ import { ProfileEditorComponent } from './profile-editor/profile-editor.componen
 
 @NgModule({
   declarations: [AppComponent, NameEditorComponent, ProfileEditorComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
